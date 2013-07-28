1d261ab0447b459aff24a117c90833f40c2e38d0
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-27 22:16:14 -0400

    Refactor out puzzle to its own class

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -1,6 +1,16 @@
 class SudokuSolver
   attr_accessor :puzzle
+
   def initialize(puzzle)
     @puzzle = puzzle
   end
+
+end
+
+class Puzzle < Array
+
+  def initialize(puzzle)
+    puzzle.each { |line| self << line }
+  end
+
 end
--- spec/sudoku_solver_spec.rb
+++ spec/sudoku_solver_spec.rb
@@ -1,8 +1,8 @@
 require_relative "../lib/sudoku_solver"
 
-describe "SudokuSolver" do
+describe "Puzzle" do
 
-  let(:puzzle) {[
+  let(:puz) {[
      [0, 0, 6, 0, 1, 0, 0, 0, 0],
      [4, 5, 1, 0, 3, 9, 2, 0, 0],
      [3, 2, 0, 0, 0, 6, 1, 0, 0],
@@ -14,11 +14,20 @@ describe "SudokuSolver" do
      [0, 0, 0, 0, 4, 0, 3, 0, 0]
   ]}
 
-  let(:solver) { SudokuSolver.new(puzzle) }
+  let(:puzzle) { Puzzle.new(puz) }
 
   it "loads a puzzle" do
-    expect(solver.puzzle[0][0]).to eql(0)
-    expect(solver.puzzle[0][2]).to eql(6)
+    expect(puzzle[0][0]).to eql(0)
+    expect(puzzle[0][2]).to eql(6)
   end
 
+  describe "SudokuSolver" do
+
+    let(:solver) { SudokuSolver.new(puzzle) }
+
+    it "takes a puzzle object" do
+      expect(solver.puzzle[0][0]).to eql(0)
+    end
+
+  end
 end
