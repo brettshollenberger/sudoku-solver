fb291168cdb427323ba4a9791985b6c28ae488b0
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-27 22:38:28 -0400

    Square knows row members

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -10,23 +10,39 @@ end
 class Puzzle < Array
 
   def initialize(puzzle)
-    puzzle.each do |line|
-      self.push(line.map { |number| Square.new(number) })
+    puzzle.each_with_index do |line, x|
+      self.push(line.each_with_index.map { |number, y| Square.new(number, x, y, self) })
     end
+  end
+
+  def values_in_row(x)
+    self[x]
   end
 
 end
 
 class Square
 
-  attr_accessor :value
+  attr_accessor :value, :x, :y, :options
 
-  def initialize(value)
+  def initialize(value, x, y, puzzle)
     @value = value
+    @x, @y = x, y
+    @puzzle = puzzle
+    compute_options unless solved? 
   end
 
   def solved?
     @value != 0
+  end
+
+  def row_members
+    @puzzle.values_in_row(@x).map { |square| square.value }.reject { |num| num == 0}
+  end
+
+protected
+  def compute_options
+    nil
   end
 
 end
--- spec/sudoku_solver_spec.rb
+++ spec/sudoku_solver_spec.rb
@@ -3,15 +3,15 @@ require_relative "../lib/sudoku_solver"
 describe "SudokuSolver" do
 
   let(:puz) {[
-       [0, 0, 6, 0, 1, 0, 0, 0, 0],
-       [4, 5, 1, 0, 3, 9, 2, 0, 0],
-       [3, 2, 0, 0, 0, 6, 1, 0, 0],
-       [2, 0, 0, 6, 9, 0, 0, 0, 8],
-       [0, 0, 0, 5, 0, 1, 0, 0, 0],
-       [9, 0, 0, 0, 2, 7, 0, 0, 1],
-       [0, 0, 7, 2, 0, 0, 0, 8, 9],
-       [0, 0, 9, 1, 7, 0, 6, 2, 5],
-       [0, 0, 0, 0, 4, 0, 3, 0, 0]
+      [0, 4, 0, 8, 0, 7, 0, 0, 2],
+      [1, 0, 8, 9, 4, 0, 7, 5, 0],
+      [3, 2, 0, 6, 0, 0, 0, 9, 0],
+      [0, 9, 0, 0, 0, 3, 8, 0, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [7, 0, 5, 4, 0, 0, 0, 3, 0],
+      [0, 5, 0, 0, 0, 4, 0, 8, 6],
+      [0, 7, 1, 0, 8, 6, 9, 0, 5],
+      [4, 0, 0, 1, 0, 5, 0, 2, 0]
     ]}
 
   let(:puzzle) { Puzzle.new(puz) }
@@ -20,15 +20,19 @@ describe "SudokuSolver" do
 
     it "knows whether it is solved or not" do
       expect(puzzle[0][0].solved?).to eql(false)
-      expect(puzzle[0][2].solved?).to eql(true)
+      expect(puzzle[0][1].solved?).to eql(true)
     end
+
+    it "knows the numbers in its row" do
+      expect(puzzle[0][0].row_members).to eql([4, 8, 7, 2])
+    end
+
+    # it "knows its available options" do
+    #   expect(puzzle[0][0].options).to eql([5, 6, 9])
+    # end
 
   end
 
   let(:solver) { SudokuSolver.new(puzzle) }
-
-  it "takes a puzzle object" do
-    expect(solver.puzzle[0][0].value).to eql(0)
-  end
 
 end
